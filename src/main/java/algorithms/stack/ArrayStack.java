ecabd78828a79a8b4180bdf6c7e688c8b524d2df
Leandro Bauret <leandro.bauret@mulesoft.com>
2023-03-23 16:22:21 -0300

    Initial commit

---
+++ src/main/java/algorithms/stack/ArrayStack.java
@@ -0,0 +1,25 @@
+package algorithms.stack;
+
+import org.jetbrains.annotations.NotNull;
+
+public class ArrayStack<E> implements Stack<E>{
+    @Override
+    public void push(@NotNull E item) {
+        throw new RuntimeException("Not implemented");
+    }
+
+    @Override
+    public E pop() {
+        throw new RuntimeException("Not implemented");
+    }
+
+    @Override
+    public boolean isEmpty() {
+        throw new RuntimeException("Not implemented");
+    }
+
+    @Override
+    public int size() {
+        throw new RuntimeException("Not implemented");
+    }
+}
---
+++ src/test/java/algorithms/stack/HelloTest.java
@@ -0,0 +1,14 @@
+package algorithms.stack;
+
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class HelloTest {
+
+
+    @Test
+    public void helloTest() {
+        assertThat("hello").isEqualTo("hello");
+    }
+}
