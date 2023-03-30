cac7f2a1403a6723ce076ee5c52467adcdeb5c5e
Santos <santosbogo@gmail.com>
2023-03-30 17:07:17 -0300

    No warnings now

--- src/main/java/algorithms/stack/ArrayStack.java
+++ src/main/java/algorithms/stack/ArrayStack.java
@@ -3,9 +3,8 @@ package algorithms.stack;
 import org.jetbrains.annotations.NotNull;
 
 public class ArrayStack<E> implements Stack<E>{
-
     private E[] stack;
-    private int point;
+    private int n;
 
     public ArrayStack(){
         stack = (E[]) new Object[10];
@@ -13,40 +12,38 @@ public class ArrayStack<E> implements Stack<E>{
     public ArrayStack(int size){
         stack = (E[]) new Object[size];
     }
-
     private void resize(int size){
-        E[] newstack = (E[]) new Object[size];
+        E[] stack2 = (E[]) new Object[size];
         for (int i = 0; i < size(); i++)
-            newstack[i] = stack[i];
-        stack = newstack;
+            stack2[i] = stack[i];
+        stack = stack2;
     }
-
     @Override
     public void push(@NotNull E item) {
-        if (size() == point){
+        if (stack.length == n){
             resize(size()*2);
         }
-        stack[point] = item;
-        point++;
+        stack[n] = item;
+        n++;
     }
-
     @Override
     public E pop() {
-        if (point == size()/4){
+        if (isEmpty()){
+            throw new IllegalStateException();
+        }
+        if (n == size()/4){
             resize(size()/2);
         }
-        E old = stack[--point];
-        stack[point] = null;
+        E old = stack[--n];
+        stack[n] = null;
         return old;
     }
-
     @Override
     public boolean isEmpty() {
-        return point == 0;
+        return n == 0;
     }
-
     @Override
     public int size() {
-        return stack.length;
+        return n;
     }
 }
