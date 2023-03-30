e2d3715882e8a536eeb5d6e8d498e0291b32e457
Santos <santosbogo@gmail.com>
2023-03-30 19:05:28 -0300

    Tests added

--- src/main/java/algorithms/stack/ArrayStack.java
+++ src/main/java/algorithms/stack/ArrayStack.java
@@ -2,48 +2,79 @@ package algorithms.stack;
 
 import org.jetbrains.annotations.NotNull;
 
-public class ArrayStack<E> implements Stack<E>{
+import java.util.Iterator;
+
+public class ArrayStack<E> implements Stack<E>, Iterable<E> {
     private E[] stack;
     private int n;
 
     public ArrayStack(){
         stack = (E[]) new Object[10];
     }
+
     public ArrayStack(int size){
         stack = (E[]) new Object[size];
     }
+
     private void resize(int size){
         E[] stack2 = (E[]) new Object[size];
         for (int i = 0; i < size(); i++)
             stack2[i] = stack[i];
         stack = stack2;
     }
+
     @Override
     public void push(@NotNull E item) {
-        if (stack.length == n){
+        if (totalspace() == n){
             resize(size()*2);
         }
         stack[n] = item;
         n++;
     }
+
     @Override
     public E pop() {
         if (isEmpty()){
             throw new IllegalStateException();
         }
-        if (n == size()/4){
-            resize(size()/2);
+        if (n == (stack.length / 4)){
+            resize(stack.length/2);
         }
         E old = stack[--n];
         stack[n] = null;
         return old;
     }
+
     @Override
     public boolean isEmpty() {
         return n == 0;
     }
+
     @Override
     public int size() {
         return n;
+    }
+
+    public int totalspace(){
+        return stack.length;
+    }
+
+    @NotNull
+    @Override
+    public Iterator<E> iterator() {
+        return new IterableArrayStack();
+    }
+
+    private class IterableArrayStack implements Iterator<E> {
+        private int pointer = n;
+        @Override
+        public boolean hasNext() {
+            return pointer > 0;
+        }
+
+        @Override
+        public E next() {
+            return stack[pointer];
+        }
     }
 }
---
+++ src/test/java/algorithms/stack/ArrayStackTests.java
@@ -0,0 +1,65 @@
+package algorithms.stack;
+
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.Assert.assertThrows;
+
+public class ArrayStackTests {
+
+    ArrayStack<Integer> testStack = new ArrayStack<>(5);
+
+    @Test
+    public void pushTest() {
+        testStack.push(2);
+        assertThat(testStack.size()).isEqualTo(1);
+    }
+
+    @Test
+    public void popTest() {
+        testStack.push(2);
+        testStack.pop();
+        assertThat(testStack.size()).isEqualTo(0);
+    }
+
+    @Test
+    public void popEmptyTest() {
+        assertThrows(IllegalStateException.class, () -> {
+            testStack.pop();
+        });
+    }
+
+    @Test
+    public void iterationTest(){
+        assertThat(testStack.iterator().hasNext()).isEqualTo(false);
+    }
+
+    @Test
+    public void loiteringTest(){
+        testStack.push(1);
+        testStack.push(2);
+        testStack.pop();
+        assertThat(testStack.iterator().next()).isEqualTo(null);
+    }
+
+    @Test
+    public void resizeTest(){
+        testStack.push(1); testStack.push(1); testStack.push(2);
+        testStack.push(2); testStack.push(1); testStack.push(2);
+
+        testStack.pop(); testStack.pop(); testStack.pop();
+        testStack.pop(); testStack.pop(); testStack.pop();
+    }
+
+    @Test
+    public void constructorsTest(){
+        ArrayStack<Integer> stack1 = new ArrayStack<>();
+        ArrayStack<Integer> stack2 = new ArrayStack<>(3);
+
+    }
+}
+//ArrayStack<Integer> stack = new ArrayStack<>();
+//        stack.push(4);
+//        Iterator<Integer> i = stack.iterator();
+//        i.hasNext();
+//        i.next();
