571c1e2e154ef723b87894e08828f39406badb78
Santos <santosbogo@gmail.com>
2023-07-07 12:28:26 -0300

    Put of red black

---
+++ src/test/java/algorithms/tree/RedBlackBinarySearchTreeTests.java
@@ -0,0 +1,8 @@
+package algorithms.tree;
+
+import java.util.Comparator;
+
+public class RedBlackBinarySearchTreeTests {
+    Comparator<Integer> comparator = Comparator.naturalOrder();
+    //RedBlackBinarySearchTree<Integer, Integer> T = new RandomizedBinarySearchTree<>(comparator);
+}
